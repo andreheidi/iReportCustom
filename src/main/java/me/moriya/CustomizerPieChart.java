50868e042f91262863b4d723804c1fdd13c9c4d9
andreheidi <aheidi.moriya@gmail.com>
2017-12-09 12:06:03 -0200

    Initial commit

---
+++ src/main/java/me/moriya/CustomizerPieChart.java
@@ -0,0 +1,19 @@
+package me.moriya;
+
+import net.sf.jasperreports.engine.JRChart;
+import net.sf.jasperreports.engine.JRChartCustomizer;
+
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot3D;
+
+public class CustomizerPieChart implements JRChartCustomizer {
+
+	public void customize(JFreeChart chart, JRChart jasperChart) {
+		PiePlot3D piePlot3D = (PiePlot3D) chart.getPlot();
+		StandardPieSectionLabelGenerator generator = new StandardPieSectionLabelGenerator("{0} {2}");
+		generator.getPercentFormat().setMaximumFractionDigits(2);
+		piePlot3D.setLabelGenerator(generator);
+	}
+
+}
